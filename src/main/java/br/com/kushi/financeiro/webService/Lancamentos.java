492abc42f3f3df5b81dfc2a74cd3fcd2b99059b8
hiro <hiro@localhost.localdomain>
2017-11-06 22:48:19 -0200

    Fist commit

---
+++ src/main/java/br/com/kushi/financeiro/webService/Lancamentos.java
@@ -0,0 +1,23 @@
+package br.com.kushi.financeiro.webService;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author hiro
+ */
+@WebService(serviceName = "Lancamentos")
+@Stateless()
+public class Lancamentos {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
